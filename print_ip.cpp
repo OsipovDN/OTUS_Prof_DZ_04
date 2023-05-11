3d6e57d53fe2b6c5583efbd1ea7d04ff84e4e3b2
OsipovDN <disorder_2@list.ru>
2023-05-11 19:11:36 +0300

    Улучшение

--- print_ip.cpp
+++ print_ip.cpp
@@ -8,33 +8,45 @@
 
 //Шаблоны для определения контейнер или нет
 template <typename T>
-auto type_v = isConteiner<T>::value;
-
+auto isConteiner_v = isConteiner<T>::value;
+//false- если не контейнер
 template <typename T>
 struct isConteiner {
-	static const bool type_value = false;
+	static const bool value = false;
 };
-
+//true- если std::vector
 template <>
 struct isConteiner <std::vector<int>> {
-	static const bool type_value = true;
+	static const bool value = true;
 };
-
+//true- если std::list
 template <>
 struct isConteiner <std::list<int>> {
-	static const bool type_value = true;
+	static const bool value = true;
 };
 
 
-
+//Шаблонная функция print для контейнеров
 template <typename T>
-void ipPrint(const T& obj, typename isConteiner < T >::type_value = 0) {
-
+void ipPrint(const T& obj, bool d = isConteiner_v <T>) {
+	if (d) {
+		int i = 0;
+		for (auto& it:obj) {
+			std::cout << it;
+			if (i != 3)
+				std::cout << ".";
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	else {
+		
+	}
 };
 
 
-template <typename T>
-std::enable_if_t<std::is_floating_point_v <T>, std::string> ipPrint(const T&) {};
+//template <typename T>
+//std::enable_if_t<std::is_floating_point_v <T>, std::string> ipPrint(const T&) {};
 
 
 
@@ -43,17 +55,21 @@ std::enable_if_t<std::is_floating_point_v <T>, std::string> ipPrint(const T&) {}
 
 int main() {
 
-	char a = -1;
+	/*char a = -1;
 	int b = 2130706433;
-	long long c = 8875824491850138409;
+	long long c = 8875824491850138409;*/
 	/*std::string f;;*/
-	std::vector <int> g = {255,255,255,255 };
+	std::vector <int> vec_ip = { 255,255,255,255 };
+	std::list<int> list_ip;
+	for (auto i = 0; i < 4; ++i)
+		list_ip.emplace_back(200 + i);
 
 	/*ipPrint(a);
 	ipPrint(b);
 	ipPrint(c);*/
 
-	ipPrint(g);
+	ipPrint(vec_ip);
+	ipPrint(list_ip);
 
 
 
