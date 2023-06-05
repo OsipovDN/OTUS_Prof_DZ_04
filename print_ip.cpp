a8db4b3c046b3fec875ebfa74b899b0e101c1f9b
OsipovDN <disorder_2@list.ru>
2023-06-05 23:20:43 +0300

    Рабочая версия для контейнеров

--- print_ip.cpp
+++ print_ip.cpp
@@ -43,7 +43,7 @@ int strToInt(std::string obj) {
 
 //Шаблон функции преобразования std::string в std::vector <int>- vec {255,255,255,255}
 template<typename T>
-std::vector<int> transform(const T& val) {
+std::vector<int> transform(T& val) {
 	std::bitset <sizeof(T) * 8> obj(val);
 	std::string bit_pul = obj.to_string();
 	std::string::size_type start = 0;
@@ -57,7 +57,7 @@ std::vector<int> transform(const T& val) {
 		vec_byte.push_back(strToInt(byte));
 		start += 8;
 	}
-	return ip;
+	return vec_byte;
 }
 
 //Шаблонная функция print для контейнеров
@@ -65,7 +65,7 @@ template <typename T>
 void ipPrint(const T& obj, bool d = isConteiner_v <T>) {
 	if (d) {
 		int i = 0;
-		for (auto& it:obj) {
+		for (auto& it : obj) {
 			std::cout << it;
 			if (i != 3)
 				std::cout << ".";
@@ -73,7 +73,7 @@ void ipPrint(const T& obj, bool d = isConteiner_v <T>) {
 		}
 		std::cout << std::endl;
 	}
-	else {
+	if (!d) {
 		
 	}
 };
@@ -87,11 +87,11 @@ int main() {
 
 	/*char a = -1;
 	int b = 2130706433;
-	long long c = 8875824491850138409;
-	
-	ipPrint(a);
-	ipPrint(b);
-	ipPrint(c);*/
+	long long c = 8875824491850138409;*/
+
+	/*ipPrint(a);
+	ipPrint(b);*/
+	//ipPrint(c);
 
 	std::vector <int> vec_ip = { 255,255,255,255 };
 	std::list<int> list_ip;
@@ -101,7 +101,7 @@ int main() {
 	ipPrint(vec_ip);
 	ipPrint(list_ip);
 
-	std::string f;
+	/*std::string f;*/
 	/*std::vector<int> res;
 	res= transform <char>(a);
 	for (auto it : res) {
