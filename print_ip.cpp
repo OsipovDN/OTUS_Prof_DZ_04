6cf23e2e8bca272d3645b09f539f795ac468ba41
OsipovDN <disorder_2@list.ru>
2023-06-06 22:40:09 +0300

    Шаблон для целочисленных

--- print_ip.cpp
+++ print_ip.cpp
@@ -14,7 +14,7 @@ template <typename T>
 struct isConteiner :std::false_type {};
 //true- если std::vector
 template <typename Alloc>
-struct isConteiner <std::vector<int,Alloc>>:std::true_type {};
+struct isConteiner <std::vector<int, Alloc>> :std::true_type {};
 //true- если std::list
 template <typename Alloc>
 struct isConteiner <std::list<int, Alloc>> :std::true_type {};
@@ -34,13 +34,13 @@ int strToInt(std::string obj) {
 //Шаблон функции преобразования std::string в std::vector <int>- vec {255,255,255,255}
 template<typename T>
 std::vector<int> transform(T& val) {
+	std::vector<int> vec_byte;
+	std::string byte;
+
 	std::bitset <sizeof(T) * 8> obj(val);
 	std::string bit_pul = obj.to_string();
 	std::string::size_type start = 0;
 	std::string::size_type stop = bit_pul.size();
-
-	std::vector<int> vec_byte;
-	std::string byte;
 
 	while (start != stop) {
 		byte = bit_pul.substr(start, 8);
@@ -48,51 +48,57 @@ std::vector<int> transform(T& val) {
 		start += 8;
 	}
 	return vec_byte;
-}
 
-//Шаблонная функция print для контейнеров
-template <typename T,bool= isConteiner_v <T>>
-void ipPrint(const T& obj) {};
-
-template <typename T>
-void ipPrint(const T& obj) {
-		int i = 0;
-		for (auto& it : obj) {
-			std::cout << it;
-			if (i != 3)
-				std::cout << ".";
-			i++;
-		}
-		std::cout << std::endl;
 };
 
-template<typename T>
-void ipPrint (std::enable_if_t<std::is_integral_v<T>,std::string>& obj) {
+//////Шаблонная функция print
+//template <typename T,bool>
+//void ipPrint(const T& obj) {};
+//
+////Специализация для контейнеров
+//template <typename T, bool = isConteiner_v<T>>
+// void ipPrint <T,true>(const T& obj) {
+//	int i = 0;
+//	for (auto& it : obj) {
+//		std::cout << it;
+//		i++;
+//		if (i != temp.size()) std::cout << ".";
+//	}
+//	std::cout << std::endl;
+//};
 
-}
+//Специализация для целочисленных переменных
+template <typename T>
+void ipPrint(T& obj) {
+	int i = 0;
+	std::vector<int> temp = transform <T>(obj);
+	for (auto& it : temp) {
+		std::cout << it;
+		i++;
+		if (i != temp.size())std::cout << ".";
+	}
+	std::cout << std::endl;
+};
 
-
-//template <typename T>
-//std::enable_if_t<std::is_integral_v <T>, std::string> ipPrint(const T&) {};
 
 
 int main() {
 
-	/*char a = -1;*/
-	/*int b = 2130706433;
-	long long c = 8875824491850138409;*/
+	char a = -1;
+	int b = 2130706433;
+	long long c = 8875824491850138409;
 
-	/*ipPrint(a);*/
-	/*ipPrint(b);*/
-	//ipPrint(c);
+	ipPrint(a);
+	ipPrint(b);
+	ipPrint(c);
 
-	std::vector <int> vec_ip = { 255,255,255,255 };
+	/*std::vector <int> vec_ip = { 255,255,255,255 };
 	std::list<int> list_ip;
 	for (auto i = 0; i < 4; ++i)
 		list_ip.emplace_back(200 + i);
 
 	ipPrint(vec_ip);
-	ipPrint(list_ip);
+	ipPrint(list_ip);*/
 
 	/*std::string f;*/
 	/*std::vector<int> res;
